4a648550c60ee376a2229b95b6031bed354758db
Raziel101 <81720812+Raziel101@users.noreply.github.com>
2026-03-19 09:03:43 -0300

    Add files via upload

--- gob_chajari_gestion_iluminacion/__manifest__.py
+++ gob_chajari_gestion_iluminacion/__manifest__.py
@@ -9,8 +9,8 @@
         'security/ir.model.access.csv',
         'views/iluminacion_views.xml',
         'views/medidor.xml',
-        #'views/mapa_action.xml',
-        #'views/mapa_luminarias_template.xml',
+        'views/mapa_action.xml',
+        'views/mapa_luminarias_template.xml',
         'views/iluminacion_menu.xml',
 
 
--- gob_chajari_gestion_iluminacion/controllers/mapa_luminarias.py
+++ gob_chajari_gestion_iluminacion/controllers/mapa_luminarias.py
@@ -4,7 +4,7 @@ import json
 
 class MapaLuminarias(http.Controller):
 
-    @http.route('/mapa/luminarias', auth='user', website=False)
+    @http.route('/mapa/luminarias', auth='user', website=True)
     def mapa_luminarias(self, **kw):
 
         luminarias = request.env['gob_chajari_gestion_iluminacion.luminaria'].sudo().search([
